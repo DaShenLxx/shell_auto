1658dd72442eec91c8b8d7004e1698bc93685679
DaShenLxx <809545012@qq.com>
2023-05-19 11:40:44 +0800

    Initial commit

---
+++ src/test/java/com/example/shell_auto/ShellAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shell_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
